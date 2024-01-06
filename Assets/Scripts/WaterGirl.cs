11901c7d5163d883e38b9191a0a78f37703f191e
JAntanasN <146197857+JAntanasN@users.noreply.github.com>
2024-01-06 22:48:03 +0200

    Initial commit

---
+++ Assets/Scripts/WaterGirl.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterGirl : MonoBehaviour
+{
+    [Header("Speed")]
+    public float speed;
+
+    [Header("JumpingForce")]
+    public float jumpForce;
+
+    private float Move;
+    private bool isGrounded;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        Move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false; 
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) 
+        {
+            isGrounded = true; 
+        }
+
+        if (collision.gameObject.CompareTag("Fireboy"))
+        {
+            // Disable collisions between the two cubes
+            Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+        }
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false; 
+        }
+    }
+
+
+}
